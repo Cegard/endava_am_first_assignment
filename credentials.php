ef48e68f7feeb782388eb79e50e17ccbe6118f08
Eduardo Galeano <eduardo.galeano@endava.com>
2018-02-02 09:37:33 -0500

    initial commit.

---
+++ credentials.php
@@ -0,0 +1,9 @@
+<?php
+	# fill with the needed information.
+	$domain_server = "";
+	$user_name = "";
+	$password = "";
+	$options = array();
+	
+	$connector = new PDO($domain_server, $user_name, $password, $options);
+?>
